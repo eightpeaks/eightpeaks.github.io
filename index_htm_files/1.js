8a8845da9ca6c322cb7fb56e05cb9bef6903369e
Dethos <34860067+eightpeaks@users.noreply.github.com>
2017-12-26 15:05:27 +0200

    Home Page

---
+++ index_htm_files/1.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_4_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "index_htm_files/266.jpg",
+ thumbnail: "index_htm_files/267.jpg"
+},
+{
+ image: "index_htm_files/268.jpg",
+ thumbnail: "index_htm_files/269.jpg"
+},
+{
+ image: "index_htm_files/270.jpg",
+ thumbnail: "index_htm_files/271.jpg"
+},
+{
+ image: "index_htm_files/272.jpg",
+ thumbnail: "index_htm_files/273.jpg"
+},
+{
+ image: "index_htm_files/274.jpg",
+ thumbnail: "index_htm_files/275.jpg"
+},
+{
+ image: "index_htm_files/276.jpg",
+ thumbnail: "index_htm_files/277.jpg"
+},
+{
+ image: "index_htm_files/278.jpg",
+ thumbnail: "index_htm_files/279.jpg"
+},
+{
+ image: "index_htm_files/280.jpg",
+ thumbnail: "index_htm_files/281.jpg"
+},
+{
+ image: "index_htm_files/282.jpg",
+ thumbnail: "index_htm_files/283.jpg"
+}
+];
---
+++ index_htm_files/2.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_5_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "index_htm_files/287.jpg",
+ thumbnail: "index_htm_files/288.jpg"
+},
+{
+ image: "index_htm_files/289.jpg",
+ thumbnail: "index_htm_files/290.jpg"
+},
+{
+ image: "index_htm_files/291.jpg",
+ thumbnail: "index_htm_files/292.jpg"
+},
+{
+ image: "index_htm_files/293.jpg",
+ thumbnail: "index_htm_files/294.jpg"
+},
+{
+ image: "index_htm_files/295.jpg",
+ thumbnail: "index_htm_files/296.jpg"
+},
+{
+ image: "index_htm_files/297.jpg",
+ thumbnail: "index_htm_files/298.jpg"
+},
+{
+ image: "index_htm_files/299.jpg",
+ thumbnail: "index_htm_files/300.jpg"
+},
+{
+ image: "index_htm_files/301.jpg",
+ thumbnail: "index_htm_files/302.jpg"
+},
+{
+ image: "index_htm_files/303.jpg",
+ thumbnail: "index_htm_files/304.jpg"
+}
+];
